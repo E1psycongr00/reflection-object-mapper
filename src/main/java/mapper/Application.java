8034baf0887c007125c88090323de4d1d7ef7fe7
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-11-23 23:18:48 +0900

    feat: application 구현

--- src/main/java/mapper/Application.java
+++ src/main/java/mapper/Application.java
@@ -1,8 +1,14 @@
 package mapper;
 
+import example.Address;
+import example.Person;
+
 public class Application {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IllegalAccessException {
+        Address address = new Address("뇽뇽로", "서울시");
+        Person person = new Person("Ace", 10, address, new String[] {"programmer", "teacher"});
+        String s = ObjectMapper.writeToString(person);
+        System.out.println(s);
     }
 }
